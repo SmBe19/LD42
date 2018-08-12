312ef2b8ae9a4d929ba54ae885be0565fa016357
Matteo Signer <matteo.signer@gmail.com>
2018-08-12 13:50:01 +0200

    add high houses

--- House.gd
+++ House.gd
@@ -6,6 +6,10 @@ const houses_small = [
 	preload("res://img/hous_small_3.png"),
 ]
 
+const houses_big = [
+	preload("res://img/House_large_1.png"),
+]
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -14,6 +18,12 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	self.texture = houses_small[randi() % len(houses_small)]
+	self.offset.y = -self.texture.get_height() / 2
+	pass
+
+func upgrade():
+	self.texture = houses_big[randi() % len(houses_big)]
+	self.offset.y = -self.texture.get_height() / 2
 	pass
 
 #func _process(delta):
