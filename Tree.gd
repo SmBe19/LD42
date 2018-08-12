4a3948db006fd4747cba360dcc96a2c088ee56a7
Matteo Signer <matteo.signer@gmail.com>
2018-08-12 10:46:07 +0200

    add tree

--- Tree.gd
+++ Tree.gd
@@ -1,5 +1,10 @@
 extends Sprite
 
+const trees = [
+	preload("res://img/tree1.png"),
+	preload("res://img/tree2.png"),
+]
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -7,6 +12,8 @@ extends Sprite
 var intersection_count = 0
 
 func _ready():
+	self.texture = trees[randi() % len(trees)]
+	self.flip_h = randi() % 2 == 0
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
